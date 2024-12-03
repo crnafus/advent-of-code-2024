0864370a20d66c020d6deabb251eaac38667b1ae
crnafus <cnafus1@gmail.com>
2024-12-03 09:49:03 -0700

    Directory structure is OK now????

---
+++ day1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace advent_of_code_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
