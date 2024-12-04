df8704a25d73ae4bc74a68b989246f33d3b3bcaf
crnafus <cnafus1@gmail.com>
2024-12-03 20:30:41 -0700

    day 2 part 2 - not working

--- day2/Program.cs
+++ day2/Program.cs
@@ -7,6 +7,7 @@ namespace day2
         static void Main(string[] args)
         {
             Get_Part1_Answer();
+            Get_Part2_Answer();
         }
 
         private static void Get_Part1_Answer()
@@ -66,7 +67,85 @@ namespace day2
             {
                 Console.WriteLine("Exception: " + e.Message);
             }
-            Console.Write("Safe Report Count: " +  safeReportCount);
+            Console.WriteLine("Part 1 - Safe Report Count: " +  safeReportCount);
+        }
+
+        private static void Get_Part2_Answer()
+        {
+            //TODO: Figure out how to read filepath from appsettings.json
+            string filePath = "C:\\Users\\Dev\\Desktop\\aoc_day2_inputs.txt";
+            string? line;
+            int safeReportCount = 0;
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    string[] values = line.Split(' ');
+                    List<string> valuesList = new List<string>(values);
+
+                    int safe = CheckIfReportIsSafe(valuesList);
+                    if(safe != -1)
+                    {
+                        valuesList.RemoveAt(safe);
+                        safe = CheckIfReportIsSafe(valuesList);
+                    }
+                    if(safe == -1)
+                    {
+                        safeReportCount++;
+                    }
+                    
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            Console.WriteLine("Part 2 - Safe Report Count: " + safeReportCount);
+        }
+        private static int CheckIfReportIsSafe(List<string> valuesList)
+        {
+            bool? ascending = null;
+            if (Int32.Parse(valuesList[1]) > Int32.Parse(valuesList[0]))
+            {
+                ascending = true;
+            }
+            else
+            {
+                ascending = false;
+            }
+
+            for (int i = 0; i < valuesList.Count - 1; i++)
+            {
+                int currentValue = Int32.Parse(valuesList[i]);
+                int nextValue = Int32.Parse(valuesList[i + 1]);
+
+                if (ascending == true)
+                {
+                    if (currentValue > nextValue || Math.Abs(currentValue - nextValue) > 3 || Math.Abs(currentValue - nextValue) < 1)
+                    {
+                        return i;
+                    }
+                }
+                else
+                {
+                    if (currentValue < nextValue || Math.Abs(currentValue - nextValue) > 3 || Math.Abs(currentValue - nextValue) < 1)
+                    {
+                        return i;
+                    }
+                }
+                //we got to the end without meeting a failing condition, so report is safe and we increment the counter.
+                if (i == valuesList.Count - 2)
+                {
+                    return -1;
+                }
+            }
+            return -1;
         }
     }
 }
