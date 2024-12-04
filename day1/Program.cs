40801e029d594931f9e6a68e31776fd5781f574f
crnafus <cnafus1@gmail.com>
2024-12-03 18:23:55 -0700

    Day 1 part 1 code - gets the right answer

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,10 +1,56 @@
-﻿namespace day1
+﻿using System.IO;
+
+namespace day1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Get_Part1_Answer();
+        }
+        private static void Get_Part1_Answer() {
+            //TODO: Figure out how to read filepath from appsettings.json
+            string filePath = "C:\\Users\\Dev\\Desktop\\aoc_day1_inputs.txt";
+            string line;
+            int totalDistance = 0;
+            List<int> leftList = new List<int>();
+            List<int> rightList = new List<int>();
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //Split line into its 2 integer lists; the .Split function is creating 2 string.isNullOrEmpty values, so we use first and last. Hacky solution :(
+                    string[] values = line.Split(' ');
+                    leftList.Add(Int32.Parse(values[0]));
+                    rightList.Add(Int32.Parse(values[3]));
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            if (leftList.Count != rightList.Count)
+            {
+                Console.WriteLine("left and right lists are not the same length");
+                return;
+            }
+
+            leftList.Sort();
+            rightList.Sort();
+
+            for (int i = 0; i < leftList.Count; i++)
+            {
+                totalDistance = totalDistance + Math.Abs(leftList[i] - rightList[i]);
+            }
+
+            Console.WriteLine("Part 1 answer - Total Distance: " + totalDistance.ToString());
         }
     }
 }
