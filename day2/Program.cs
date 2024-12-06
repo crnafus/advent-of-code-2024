71c3c57ce94c693b50c374d2187b377d485ee420
crnafus <cnafus1@gmail.com>
2024-12-05 22:37:02 -0700

    day 2 part 2 working solution

--- day2/Program.cs
+++ day2/Program.cs
@@ -24,41 +24,13 @@ namespace day2
                 //Continue to read until you reach end of file
                 while (line != null)
                 {
-                    string[] values = line.Split(' ');
-                    bool? ascending = null;
-                    if (Int32.Parse(values[1]) > Int32.Parse(values[0]))
+                    List<int> valuesList = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                    bool isSafe = CheckIfSafe(valuesList);
+                    if (isSafe)
                     {
-                        ascending = true;
-                    }
-                    else
-                    {
-                        ascending = false;
+                        safeReportCount++;
                     }
 
-                    for (int i = 0; i < values.Length - 1; i++)
-                    {
-                        int currentValue = Int32.Parse(values[i]);
-                        int nextValue = Int32.Parse(values[i + 1]);
-                        if (ascending == true)
-                        {
-                            if (currentValue > nextValue || Math.Abs(currentValue - nextValue) > 3 || Math.Abs(currentValue - nextValue) < 1)
-                            {
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            if (currentValue < nextValue || Math.Abs(currentValue - nextValue) > 3 || Math.Abs(currentValue - nextValue) < 1)
-                            {
-                                break;
-                            }
-                        }
-                        //we got to the end without meeting a failing condition, so report is safe and we increment the counter.
-                        if(i == values.Length - 2)
-                        {
-                            safeReportCount++;
-                        }
-                    }
                     line = sr.ReadLine();
                 }
                 sr.Close();
@@ -67,7 +39,7 @@ namespace day2
             {
                 Console.WriteLine("Exception: " + e.Message);
             }
-            Console.WriteLine("Part 1 - Safe Report Count: " +  safeReportCount);
+            Console.WriteLine("Part 1 - Safe Report Count: " + safeReportCount);
         }
 
 
@@ -85,46 +57,22 @@ namespace day2
                 //Continue to read until you reach end of file
                 while (line != null)
                 {
-                    string[] values = line.Split(' ');
-                    List<string> valuesList = new List<string>(values);
-                    bool continueChecks = true;
-
-                    bool hasMultipleDirectionChanges = HasMultipleDirectionChanges(valuesList);
-
-
-                    //If the ascend/descend changes more than once, that means at least two numbers would have to be removed for it to be safe.
-                    if (hasMultipleDirectionChanges)
+                    List<int> valuesList = line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+                    if(CheckIfSafe(valuesList))
                     {
-                        continueChecks = false;
+                        safeReportCount++;
                     }
-
-                    if (continueChecks)
+                    else
                     {
-                        bool hasOneDirectionChange = CheckForOneDirectionChange(valuesList);
-                        if (hasOneDirectionChange)
+                        for (int i = 0; i < valuesList.Count; i++)
                         {
-                            GetDirectionChangeIndex(valuesList);
-                            int directionChangeIndex = GetDirectionChangeIndex(valuesList);
-                            if (directionChangeIndex != -1)
-                            {
-                                Console.WriteLine("Values list before removal: " + string.Join(", ", valuesList));
-                                valuesList.RemoveAt(directionChangeIndex);
-                                Console.WriteLine("Values List after removal: " + string.Join(", ", valuesList));
-                            }
-                            bool isReportSafe = CheckIfSafe(valuesList);
-                            if (isReportSafe)
+                            List<int> valuesListCopy = valuesList.ToList();
+                            valuesListCopy.RemoveAt(i);
+                            if(CheckIfSafe(valuesListCopy))
                             {
                                 safeReportCount++;
+                                break;
                             }
-                        }
-                        else
-                        {
-                            bool isReportSafe = CheckIfSafe(valuesList);
-                            if (isReportSafe)
-                            {
-                                safeReportCount++;
-                            }
-
                         }
                     }
                     line = sr.ReadLine();
@@ -137,108 +85,10 @@ namespace day2
             }
             Console.WriteLine("Part 2 - Safe Report Count: " + safeReportCount);
         }
-        
-        private static bool HasMultipleDirectionChanges(List<string> valuesList)
+        private static bool CheckIfSafe(List<int> valuesList)
         {
-            int numAscends = 0;
-            int numDescends = 0;
-            for (int i = 0; i < valuesList.Count - 1; i++)
-            {
-                if (Int32.Parse(valuesList[i]) < Int32.Parse(valuesList[i + 1]))
-                {
-                    numAscends++;
-                }
-                else
-                {
-                    numDescends++;
-                }
-            }
-            if(numAscends > 1 && numDescends > 1)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        private static bool CheckForOneDirectionChange(List<string> valuesList)
-        {
-            int numAscends = 0;
-            int numDescends = 0;
-            for (int i = 0; i < valuesList.Count - 1; i++)
-            {
-                if (Int32.Parse(valuesList[i]) < Int32.Parse(valuesList[i + 1]))
-                {
-                    numAscends++;
-                }
-                else
-                {
-                    numDescends++;
-                }
-            }
-            if (numAscends == 1 || numDescends == 1)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        private static int GetDirectionChangeIndex(List<string> valuesList)
-        {
-            int index = -1;
-            int numAscends = 0;
-            int numDescends = 0;
-            bool ascending = true;
-            for (int i = 0; i < valuesList.Count - 1; i++)
-            {
-                if (Int32.Parse(valuesList[i]) < Int32.Parse(valuesList[i + 1]))
-                {
-                    numAscends++;
-                }
-                else
-                {
-                    numDescends++;
-                }
-            }
-            if (numAscends > numDescends)
-            {
-                ascending = true;
-            }
-            else
-            {
-                ascending = false;
-            }
-            for (int i = 0; i < valuesList.Count - 1; i++)
-            {
-                if (ascending == true)
-                {
-                    if (Int32.Parse(valuesList[i]) > Int32.Parse(valuesList[i+1]))
-                    {
-                        index = i+1;
-                        break;
-                    }
-                }
-                else
-                {
-                    if (Int32.Parse(valuesList[i]) < Int32.Parse(valuesList[i + 1]))
-                    {
-                        index = i + 1;
-                        break;
-                    }
-                }
-            }
-            return index;
-        }
-
-        private static bool CheckIfSafe(List<string> valuesList)
-        {
-            bool isSafe = false;
             bool? ascending = null;
-            if (Int32.Parse(valuesList[1]) > Int32.Parse(valuesList[0]))
+            if (valuesList[0] < valuesList[1])
             {
                 ascending = true;
             }
@@ -249,32 +99,25 @@ namespace day2
 
             for (int i = 0; i < valuesList.Count - 1; i++)
             {
-                int currentValue = Int32.Parse(valuesList[i]);
-                int nextValue = Int32.Parse(valuesList[i + 1]);
+                int currentValue = valuesList[i];
+                int nextValue = valuesList[i + 1];
+                
                 if (ascending == true)
                 {
                     if (currentValue > nextValue || Math.Abs(currentValue - nextValue) > 3 || Math.Abs(currentValue - nextValue) < 1)
                     {
-                        isSafe = false;
-                        break;
+                        return false;
                     }
                 }
                 else
                 {
                     if (currentValue < nextValue || Math.Abs(currentValue - nextValue) > 3 || Math.Abs(currentValue - nextValue) < 1)
                     {
-                        isSafe = false;
-                        break;
+                        return false;
                     }
                 }
-                //we got to the end without meeting a failing condition, so report is safe and we increment the counter.
-                if (i == valuesList.Count - 2)
-                {
-                    isSafe = true;
-                    break;
-                }
             }
-            return isSafe;
+            return true;
         }
     }
 }
